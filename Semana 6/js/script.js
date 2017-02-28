a19e4b1e4a188214f4560eff6527a4041246290f
perlafdez <perlafernandezf@gmail.com>
2017-02-28 18:12:00 -0400

    Tarea Semana 6

---
+++ Semana 6/js/script.js
@@ -0,0 +1,7 @@
+
+//Set up the fancybox plugin
+console.log('Initializing the FancyBox plugin');
+$(document).ready(function() {
+		$(".fancybox").fancybox();
+		console.log('FancyBox Initialized');
+	});
