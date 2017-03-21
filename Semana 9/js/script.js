41ab04bab87f59e700ab9915e26009e0557ace8a
perlafdez <perlafernandezf@gmail.com>
2017-03-21 18:38:26 -0400

    Tarea Semana 9

---
+++ Semana 9/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready (function(){
+	
+	$('.landingh3').typeIt({
+	 strings: ['Ask a yes or no question.', 'Refresh to reveal the answer.'],	
+     breakLines: false,
+     autoStart: false,
+     speed: 60
+	});
+});
