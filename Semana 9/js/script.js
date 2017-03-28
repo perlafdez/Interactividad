de7d9df91cc76659ed2dd42b45d5cfe7a7e9a95a
perlafdez <perlafernandezf@gmail.com>
2017-03-28 18:22:26 -0400

    Formulario

--- Semana 9/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready (function(){
-	
-	$('.landingh3').typeIt({
-	 strings: ['Ask a yes or no question.', 'Refresh to reveal the answer.'],	
-     breakLines: false,
-     autoStart: false,
-     speed: 60
-	});
-});
